d8d5c2451b7af0554170d5ce106019c0a3386f1e
thangSu <phamthang3003@gmail.com>
2022-07-12 23:16:21 +0700

    add bash script

---
+++ install_package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# update system
+sudo yum update -y
+#install jenkins
+
+# install docker
+sudo apt install docker -y
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+# install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+#install conntrack
+sudo apt install conntrack -y
+
+#install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+#install helm 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
